5736c2206210c7ca201783666dcdddbfeb3898af
KYLIN <kylin801122@gmail.com>
2020-11-17 22:39:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-import os
 
 app = Flask(__name__)
 
@@ -33,32 +32,30 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
-    # 回傳google地圖
-    if "聯成" in user_message:
-        message = LocationSendMessage(title="聯成電腦",
-                                      address="234新北市永和區永和路二段59號4樓",
-                                      latitude=25.011189,
-                                      longitude=121.514564)
-        line_bot_api.reply_message(event.reply_token, message)
-    # 回傳特定訊息
+    # 使用者輸入"貓咪"，訊息為文字訊息
     if "貓咪" in user_message:
         message = TextSendMessage(text="給我罐頭 !")
-        line_bot_api.reply_message(event.reply_token, message)
     # 回傳圖片訊息
     if "圖片" in user_message:
         # preview_image_url預覽縮圖、original_content_url實際開啟圖
-        message = ImageSendMessage(original_content_url='https://cdn.hk01.com/di/media/images/564720/org/7a5b31ccd89a2360794c1ef6bf54393f.jpg/0ws2YFTJcguqJ5hF1Hp3V8ELwZfAP_rMiLU2UYi1NlE?v=w1920',
-                                   preview_image_url='https://storage.googleapis.com/www-cw-com-tw/article/201810/article-5bd182cf13ebb.jpg')
-        line_bot_api.reply_message(event.reply_token, message)
+        message = ImageSendMessage(original_content_url="https://imgur.com/qTQOkol",
+                                   preview_image_url="https://imgur.com/qTQOkol")
+    # 使用者輸入"地圖"，訊息為google地圖
+    if "地圖" in user_message:
+        message = LocationSendMessage(title="猴硐貓村",
+                                      address="224新北市瑞芳區侯硐",
+                                      latitude=25.088678,
+                                      longitude=121.827537)
     # 回傳影片訊息
     if "影片" in user_message:
+        # preview_image_url影片預覽縮圖、original_content_url實際開啟影片
         message = VideoSendMessage(original_content_url='https://video-previews.elements.envatousercontent.com/h264-video-previews/4103404.mp4',
-                                   preview_image_url='https://cdn.hk01.com/di/media/images/564720/org/7a5b31ccd89a2360794c1ef6bf54393f.jpg/0ws2YFTJcguqJ5hF1Hp3V8ELwZfAP_rMiLU2UYi1NlE?v=w1920')
-        line_bot_api.reply_message(event.reply_token, message)
+                                   preview_image_url='https://imgur.com/qTQOkol')
     # 回傳使用者輸入內容
     if"喵" in user_message:
         message = TextSendMessage(text=user_message)
-        line_bot_api.reply_message(event.reply_token, message)
+    # 回傳訊息內容
+    line_bot_api.reply_message(event.reply_token, message)
 
 
 # 主程式
